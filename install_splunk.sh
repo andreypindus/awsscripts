c56f5373ae7a30343374f0d20cdd1264b58086da
Andrey Pindus <andreypindus@Andreys-MacBook-Pro.local>
2021-11-11 10:21:25 +0100

    first commit

---
+++ install_splunk.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+adminpass=$1
+
+export SPLUNK_USER=splunk
+export SPLUNK_HOME=/opt/splunk
+
+#Install splunk
+wget "https://apindus-test.s3.eu-west-2.amazonaws.com/splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm"
+rpm -ivh splunk-8.2.3-cd0848707637-linux-2.6-x86_64.rpm
+cat > $SPLUNK_HOME/etc/system/local/user-seed.conf <<EOF
+[user_info]
+USERNAME = admin
+PASSWORD = $adminpass
+EOF
+useradd $SPLUNK_USER
+groupadd $SPLUNK_USER
+chown -R splunk:splunk $SPLUNK_HOME
+sudo -u $SPLUNK_USER /opt/splunk/bin/splunk start --accept-license
+
+
