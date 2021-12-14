dc5b56241420f3c1f728c74bc6564feabbfdaa73
Andrey Pindus <andrey.pin@gmail.com>
2021-12-14 12:52:31 +0100

    Update install_splunk.sh

--- install_splunk.sh
+++ install_splunk.sh
@@ -16,5 +16,4 @@ useradd $SPLUNK_USER
 groupadd $SPLUNK_USER
 chown -R splunk:splunk $SPLUNK_HOME
 sudo -u $SPLUNK_USER /opt/splunk/bin/splunk start --accept-license
-
-
+sudo -u $SPLUNK_USER /opt/splunk/bin/splunk boot-start
