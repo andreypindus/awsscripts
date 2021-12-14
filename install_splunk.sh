162413a4844aab7d84a1bbb4fbadb5080ef89ae4
Andrey Pindus <andrey.pin@gmail.com>
2021-12-14 13:38:37 +0100

    Update install_splunk.sh

--- install_splunk.sh
+++ install_splunk.sh
@@ -12,8 +12,9 @@ cat > $SPLUNK_HOME/etc/system/local/user-seed.conf <<EOF
 USERNAME = admin
 PASSWORD = $adminpass
 EOF
+/opt/splunk/bin/splunk enable boot-start
 useradd $SPLUNK_USER
 groupadd $SPLUNK_USER
 chown -R splunk:splunk $SPLUNK_HOME
 sudo -u $SPLUNK_USER /opt/splunk/bin/splunk start --accept-license
-sudo -u $SPLUNK_USER /opt/splunk/bin/splunk enable boot-start
+/opt/splunk/bin/splunk enable boot-start
